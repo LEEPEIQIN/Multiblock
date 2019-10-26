08aa7ad66a0e3f75239425c410168caa18c839d3
LEEPEIQIN <47051186+LEEPEIQIN@users.noreply.github.com>
2019-10-27 00:31:54 +0800

    Update Net2.py

--- Net2.py
+++ Net2.py
@@ -841,9 +841,9 @@ net2.to(device)
 #first:
 criterion = nn.L1Loss()
 optimizer=torch.optim.Adam(net2.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
-for epoch in range(10):
+for epoch in range(100):
     running_loss=0.0
-    for i in range(100):
+    for i in range(200):
         HR,LR=generator()
         HR=HR.to(device)
         LR=LR.to(device)
@@ -882,12 +882,12 @@ for epoch in range(10):
     torch.save(net2.state_dict(), 'real_net2.pt')
 print('Finished Training phase1')
 
-#next using L2, and adam:
-criterion = nn.MSELoss()
+#next using L1, and adam:
+criterion = nn.L1Loss()
 optimizer=torch.optim.Adam(net2.parameters(), lr=0.00001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
-for epoch in range(200):
+for epoch in range(100):
     running_loss=0.0
-    for i in range(100):
+    for i in range(200):
         HR,LR=generator()
         HR=HR.to(device)
         LR=LR.to(device)
