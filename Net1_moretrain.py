4278435f2338fed2a149f622e0bd732e562dc30c
LEEPEIQIN <47051186+LEEPEIQIN@users.noreply.github.com>
2019-10-27 16:12:37 +0800

    Update Net1_moretrain.py

--- Net1_moretrain.py
+++ Net1_moretrain.py
@@ -488,11 +488,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 net1.to(device)
 
 
-criterion = nn.MSELoss()
-optimizer=torch.optim.Adam(net1.parameters(), lr=0.00001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+criterion = nn.L1Loss()
+optimizer=torch.optim.Adam(net1.parameters(), lr=0.000001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
 for epoch in range(200):
     running_loss=0.0
-    for i in range(500):
+    for i in range(300):
         HR,LR=generator()
         HR=HR.to(device)
         LR=LR.to(device)
